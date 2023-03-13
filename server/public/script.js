fee085710983ec98a3ed6bb5e499d2ff862974b1
ngimdock <ngimdockzemfack@gmail.com>
2023-03-13 06:59:03 +0100

    Configure stripe

--- server/public/script.js
+++ server/public/script.js
@@ -1,5 +1,26 @@
 const checkoutBtn = document.querySelector("#checkout-btn");
 
 checkoutBtn.addEventListener("click", () => {
-  console.log("Checkout button clicked");
+  fetch("/create-checkout-session", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      items: [
+        { id: 1, quantity: 3 },
+        { id: 2, quantity: 1 },
+      ],
+    }),
+  })
+    .then((res) => {
+      if (res.ok) return res.json();
+      res.json().then((json) => Promise.reject(json));
+    })
+    .then(({ url }) => {
+      window.location = url;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
--- server/server.js
+++ server/server.js
@@ -6,7 +6,10 @@ dotenv.config({ path: "../.env" });
 
 const app = express();
 app.use(express.json());
-const port = 8000;
+app.use(express.static("public"));
+const port = process.env.SERVER_PORT | 8000;
+
+console.log(`${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`);
 
 const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY);
 
@@ -16,7 +19,40 @@ const storeItems = new Map([
   ["3", { priceInCents: 3000, name: "Shoes" }],
 ]);
 
-console.log({ stripeKey: process.env.STRIPE_PRIVATE_KEY });
+app.post("/create-checkout-session", async (req, res) => {
+  const items = req.body.items;
+
+  const lineItems = items.map(({ id, quantity }) => {
+    const storeItem = storeItems.get(id);
+
+    console.log({ storeItem });
+
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: storeItem.name,
+        },
+
+        unit_amount: storeItem.priceInCents,
+      },
+      quantity: quantity,
+    };
+  });
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: lineItems,
+      success_url: `${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/success.html`,
+      cancel_url: `${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/cancel.html`,
+    });
+    res.json({ url: session.url });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
